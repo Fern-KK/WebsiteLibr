e163e6b5cfae44ec83a236844dfd75c665cd03b7
Paproc <too173poo@gmail.com>
2025-06-10 23:20:16 +0200

    dużo rzeczy

--- skrypt.js
+++ skrypt.js
@@ -2,50 +2,39 @@
 function lightmode() {
     document.body.classList.remove('darkMode', 'yellowMode');
     document.body.classList.add('lightMode');
-    setCookie('mode', '0');
+    localStorage.setItem("Mode", 0);
 }
 function darkmode() {
     document.body.classList.remove('lightMode', 'yellowMode');
     document.body.classList.add('darkMode');
-    setCookie('mode', '1');
+    localStorage.setItem("Mode", 1);
 }
 function yellowmode() {
     document.body.classList.remove('darkMode', 'lightMode');
     document.body.classList.add('yellowMode');
-    setCookie('mode', '2');
+    localStorage.setItem("Mode", 2);
 }
 
 // to samo co wczesnie ale dla wielkosci tekstu
 function textNormal() {
     document.body.classList.remove('bigText', 'biggerText');
     document.body.classList.add('normalText');
-    setCookie('font', '0');
+    localStorage.setItem("Font", 0);
 }
 
 function textMiddle() {
     document.body.classList.remove('normalText', 'biggerText');
     document.body.classList.add('bigText');
-    setCookie('font', '1');
+    localStorage.setItem("Font", 1);
 }
 
 function textBig() {
     document.body.classList.remove('normalText', 'bigText');
     document.body.classList.add('biggerText');
-    setCookie('font', '2');
+    localStorage.setItem("Font", 2);
 }
 
-// do ustawiania cookies
-function setCookie(name, value) {
-    document.cookie = `${name}=${value}; path=/; max-age=86400`;
-}
 
-// zalaczanie domyslnych ustawien
-if (!document.cookie.includes('mode=')) {
-    setCookie('mode', '0');
-}
-if (!document.cookie.includes('font=')) {
-    setCookie('font', '0');
-}
 
 // wykrywa klikniecie i wywoluje funkcie kontrastu i czciosnki
 document.getElementById('light').addEventListener('click', lightmode);
@@ -56,27 +45,14 @@ document.getElementById('normalText').addEventListener('click', textNormal);
 document.getElementById('bigText').addEventListener('click', textMiddle);
 document.getElementById('biggerText').addEventListener('click', textBig);
 
-// sprawdza cookies
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
 
 
-
-
-
-
-
-// kontrast
-const mode = getCookie('mode');
+const mode = localStorage.getItem("Mode");
 if (mode === '0') lightmode();
 else if (mode === '1') darkmode();
 else if (mode === '2') yellowmode();
 
-// czcionski
-const font = getCookie('font');
+const font = localStorage.getItem("Font");
 if (font === '0') textNormal();
 else if (font === '1') textMiddle();
 else if (font === '2') textBig();
@@ -86,15 +62,75 @@ else if (font === '2') textBig();
 
 
 
-let books;
-async function fetchContent() {
-    const response = await fetch('https://backend-www-projekt.onrender.com/books');
-    const data = await response.json();
-    books = data.books;
-    lista();
+// sprawdza czy jaki jest dzień i godz i wyświetla czy biblioteka jest teraz otwarta
+function checkTime() {
+    const now = new Date();
+    const dayOfWeek = now.getDay();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const currentTime = hours * 100 + minutes;
+
+    let state = "Biblioteka jest zamknięta"; 
+    
+    // Sprawdzamy dni (pon-pt)
+    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+        if (currentTime >= 800 && currentTime < 1900) {
+            state = "Biblioteka jest otwarta";}
+    }
+    // sobota
+    else if (dayOfWeek === 6 && currentTime >= 800 && currentTime < 1400) {
+        state = "Biblioteka jest otwarta";
+    }
+    const IsOpen = document.getElementById("IsOpen");
+    IsOpen.style.fontWeight = 'bold';
+    IsOpen.textContent = state;
 }
-fetchContent();
-function lista() {
+
+// Co minute odpala checkTime
+if(document.getElementById("IsOpen")){
+    checkTime();
+    setInterval(checkTime, 60000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(document.getElementById("katalog")){
+   
+    let books;
+    async function fetchContent() {
+        const response = await fetch('https://backend-www-projekt.onrender.com/books');
+        const data = await response.json();
+        books = data.books;
+        lista();
+    }
+    fetchContent();
+    function lista() {
     // usuwanie ladowania
     document.getElementById("loading").remove();
     books.forEach(element => {
@@ -103,6 +139,24 @@ function lista() {
         // Główny div
         const bookDiv = document.createElement("div");
         bookDiv.className = "book";
+        // dodwananie atrybutów do fitrowania
+        bookDiv.setAttribute("title",element.title);
+        bookDiv.setAttribute("author",element.author);        
+        bookDiv.setAttribute("genre",element.genre);        
+        bookDiv.setAttribute("available",element.available);
+        bookDiv.setAttribute("ebook",element.ebook);
+        if(element.year<476 ){bookDiv.setAttribute("year","pne");}
+        if(element.year>=476 && element.year<1500){bookDiv.setAttribute("year","sr");}
+        if(element.year>=1500 && element.year<1600){bookDiv.setAttribute("year","16");}
+        if(element.year>=1600 && element.year<1700){bookDiv.setAttribute("year","17");}
+        if(element.year>=1700 && element.year<1800){bookDiv.setAttribute("year","18");}
+        if(element.year>=1800 && element.year<1900){bookDiv.setAttribute("year","19");}
+        if(element.year>=1900 && element.year<2000){bookDiv.setAttribute("year","20");}
+        if(element.year>=2000 && element.year<2100){bookDiv.setAttribute("year","21");}
+        
+
+
+
         
         // div z indoramcjami o książce
         const infoDiv = document.createElement("div");
@@ -111,6 +165,8 @@ function lista() {
         const title = document.createElement("h3");
         title.textContent = element.title;
         infoDiv.appendChild(title);
+        
+
         
         // Autor i rok 
         const authorYear = document.createElement("p");
@@ -160,60 +216,140 @@ function lista() {
         
         // Dodanie diva do article
         document.getElementById("katalog").appendChild(bookDiv);
+        
     });
-}
+}}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// sprawdza czy jaki jest dzień i godz i wyświetla czy biblioteka jest teraz otwarta
-function checkTime() {
-    const now = new Date();
-    const dayOfWeek = now.getDay();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const currentTime = hours * 100 + minutes;
-
-    let state = "Biblioteka jest zamknięta"; 
+// filtrowanie
+function applyFilters() {
+    const dostep = document.getElementsByName('available')[0].checked;
+    const ebook = document.getElementsByName('ebook')[0].checked;
+    const yearInput = document.getElementById("yearFilter").value;
+    const genreInput = document.getElementById("genreFilter").value;
     
-    // Sprawdzamy dni (pon-pt)
-    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-        if (currentTime >= 800 && currentTime < 1900) {
-            state = "Biblioteka jest otwarta";}
-    }
-    // sobota
-    else if (dayOfWeek === 6 && currentTime >= 800 && currentTime < 1400) {
-        state = "Biblioteka jest otwarta";
-    }
-    const IsOpen = document.getElementById("IsOpen");
-    IsOpen.style.fontWeight = 'bold';
-    IsOpen.textContent = state;
-}
+    const books = document.querySelectorAll(".book");
+    
+    books.forEach(book => {
+        let showBook = true;
 
-// Co minute odpala checkTime
-setInterval(checkTime, 60000);
-checkTime();
+        // Apply each filter only if it's active
+        if (dostep) {
+            showBook = showBook && book.getAttribute("available") === "true";
+        }
+        
+        if (ebook) {
+            showBook = showBook && book.getAttribute("ebook") !== "brak";
+        }
+        //sprawdza czy jest wartość yearInput i czy nie jest all
+        if (yearInput && yearInput !== "all") {
+            showBook = showBook && book.getAttribute("year") === yearInput;
+        }
+        
+        if (genreInput && genreInput !== "all") {
+            showBook = showBook && book.getAttribute("genre") === genreInput;
+        }
+        
+
+        if(showBook){
+            book.style.display="flex" 
+        }else{book.style.display="none" }
+    });
+};
+
+document.getElementById("filtr").addEventListener('click', applyFilters);
+
+
+//reset
+document.getElementById("resetFilters").addEventListener('click', function() {
+    document.getElementsByName('available')[0].checked = false;
+    document.getElementsByName('ebook')[0].checked = false;
+    document.getElementById("yearFilter").value = "all";
+    document.getElementById("genreFilter").value = "all";
+    applyFilters();
+});
+
+
+
+
+
+
+
+
+
+// function applyFilters() {
+//     const dostep = document.getElementsByName('available')[0].value;
+//     const ebook = document.getElementsByName('ebook')[0].value;
+//     const yearImput = document.getElementsById("yearFilter").value;
+//     const genreImput = document.getElementsById("genreFilter").value;
+
+//     // const search = document.searchInput.value.toUpperCase();
+    
+//     const books = document.querySelectorAll(".book");
+//     books.forEach(book => {
+//         // const info = (book.getAttribute("title")+book.getAttribute("author")+book.getAttribute("genre")+book.getAttribute("year")).toUpperCase()
+//         let showBook = true;
+
+
+//         if(dostep.checked===true){
+//             if(book.getAttribute("available")){
+//                 showBook=true;
+//             }else{showBook=false;}}
+
+//         if(ebook.checked===true){
+//             if(book.getAttribute("ebook")==="brak"){
+//                 showBook=false;
+//             }else{showBook=true;}}
+
+//         if(yearImput===book.getAttribute("year")){
+//             showBook=true;
+//         }else{showBook=false;}
+
+//         if(genreImput===book.getAttribute("genre")){
+//             showBook=true;
+//         }else{showBook=false;}    
+
+        
+//         // if(info.includes(search)){
+//         //     if(book.getAttribute(available)){
+//         //         book.style.display = "flex"
+//         //     }else{book.style.display = "none"}}
+//         // else{book.style.display = "flex"}
+
+//         if(showBook){book.style.display = "flex"}
+//         else{book.style.display = "none"}
+        
+
+
+
+//     })
+// };
+
+// document.getElementById("filtr").addEventListener('click', applyFilters);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
